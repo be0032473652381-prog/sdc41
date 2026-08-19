f3d9764d796befaa7600114083147554412c5396
Henrik Andersen <be0032473652381@gmail.com>
2026-08-19 12:58:21 +0200

    Add .gitignore, stop tracking build artifacts

--- build/CMakeFiles/sdc41.dir/src/sdc41_crc.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/sdc41.dir/src/sdc41_crc.c.o: \
- /home/henrik/projects/sdc41/src/sdc41_crc.c \
- /home/henrik/projects/sdc41/src/sdc41_crc.h \
- /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
- /usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
- /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h
